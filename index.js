6e9293b95db9f98828acd51667908aa63b43fbc0
Serhii Biruk <sergy.bestr@gmail.com>
2021-04-23 20:21:57 +0300

    add Poster

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ server.post('/get-movie-details', (req, res) => {
             if (!req.body.queryResult.parameters.genre && !req.body.queryResult.parameters.year && !req.body.queryResult.parameters.director
                 && !req.body.queryResult.parameters.actors && !req.body.queryResult.parameters.plot && !req.body.queryResult.parameters.rating){
 
-                    dataToSend+=  `${movie.Poster}\nGenre: ${movie.Genre}.\nYear: ${movie.Year}.\nDirector: ${movie.Director}.\nActors: ${movie.Actors}.\nPlot: ${movie.Plot}.\nRating: ${movie.imdbRating}.`;
+                    dataToSend+=  `Genre: ${movie.Genre}.\nYear: ${movie.Year}.\nDirector: ${movie.Director}.\nActors: ${movie.Actors}.\nPlot: ${movie.Plot}.\nRating: ${movie.imdbRating}.\n${movie.Poster}`;
                 }
             return res.json({
                 fulfillmentText: dataToSend,
