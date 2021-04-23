83be19f4a1ab3a6373ff27b0a8b75229360e0858
Serhii Biruk <sergy.bestr@gmail.com>
2021-04-23 18:43:23 +0300

    Translate some words

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ server.post('/get-movie-details', (req, res) => {
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `Назва: ${movie.Title}\nАктори: ${movie.Actors}\nЖанр: genre[${movie.Genre}[0]]\nРік: ${movie.Year}`;
+            var en_genre = ${movie.Genre}
+            dataToSend += `Назва: ${movie.Title}\nАктори: ${movie.Actors}\nЖанр:en_genre.split(',')\nРік: ${movie.Year}`;
 
             return res.json({
                 fulfillmentText: dataToSend,
