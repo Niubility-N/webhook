d35e695b85309038991f6249f28a1e9a4c30900b
Serhii Biruk <sergy.bestr@gmail.com>
2021-04-23 20:07:57 +0300

    Translate some words

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
-const API_KEY = require('./apiKey');
+const API_KEY = '46617882';
 
 const server = express();
 server.use(bodyParser.urlencoded({
@@ -13,7 +13,7 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
-    var genre = {'бойовик':'Action','кримінал': 'Crime', 'драма':'Drama','трилер':'Thriller'}
+
     const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
@@ -23,10 +23,33 @@ server.post('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            var en_genre = `${movie.Genre}`;
-            dataToSend += `Назва: ${movie.Title}\nАктори: ${movie.Actors}\nЖанр:`+ genre.get('Action')+`\nРік: ${movie.Year}`;
-
+            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info 
+                on that. Here's some info on 'The Godfather' instead.\n` : '';
+            
+            dataToSend += `Title: ${movie.Title}.\n`;
+           
+            if (req.body.queryResult.parameters.genre){
+                dataToSend+=`Genre: ${movie.Genre}.\n`;
+            }
+            if (req.body.queryResult.parameters.year){
+                dataToSend+=`Year: ${movie.Year}.\n`;
+            }
+            if (req.body.queryResult.parameters.director){
+                dataToSend+=`Director: ${movie.Director}.\n`;
+            }
+            if (req.body.queryResult.parameters.actors){
+                dataToSend+=`Actors: ${movie.Actors}.\n`;
+            }
+            if (req.body.queryResult.parameters.plot){
+                dataToSend+=`Plot: ${movie.Plot}.\n`;
+            }
+            if (req.body.queryResult.parameters.rating){
+                dataToSend+=`Rating: ${movie.imdbRating}.\n`;
+            }
+            if (!req.body.queryResult.parameters.genre && !req.body.queryResult.parameters.year && !req.body.queryResult.parameters.director
+                && !req.body.queryResult.parameters.actors && !req.body.queryResult.parameters.plot && !req.body.queryResult.parameters.rating){
+                    dataToSend+=`Genre: ${movie.Genre}.\nYear: ${movie.Year}.\nDirector: ${movie.Director}.\nActors: ${movie.Actors}.\nPlot: ${movie.Plot}.\nRating: ${movie.imdbRating}.`;
+                }
             return res.json({
                 fulfillmentText: dataToSend,
                 source: 'get-movie-details'
