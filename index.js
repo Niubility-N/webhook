070781636b90f18713f909099d416f060e721200
Serhii Biruk <sergy.bestr@gmail.com>
2021-04-25 14:31:58 +0300

    some correct

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ server.post('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info 
-                on that. Here's some info on 'The Godfather' instead.\n` : '';
+            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             
             dataToSend += `Title: ${movie.Title}.\n`;
            
