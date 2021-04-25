8b1e0a1cd31ffb61b792a8981d011dc8396ecf4a
Serhii Biruk <sergy.bestr@gmail.com>
2021-04-25 14:56:46 +0300

    some correct

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ server.post('/get-movie-details', (req, res) => {
             const movie = JSON.parse(completeResponse);
             let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
             
-            if (movie.Title != 'undefined'){
+            if (movie.Title){
             
                 dataToSend += `Title: ${movie.Title}.\n`;
             
