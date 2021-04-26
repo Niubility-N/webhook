69ec7ed4007c76e4b87f2b26baf67bed12a39ca6
Serhii Biruk <sergy.bestr@gmail.com>
2021-04-26 12:48:52 +0300

    add corect

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
 
-    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'The Godfather';
+    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'INCORRECT';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
@@ -23,44 +23,44 @@ server.post('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
+            let dataToSend = movieToSearch === 'INCORRECT' ? `I don't have the required info on that. Please check that the data entered is correct.\n Example of input: What do you know about "Suicide squad"?` : '';
             
             if (movie.Title){
             
                 dataToSend += `🎬Title: ${movie.Title}.\n`;
            
                 if (req.body.queryResult.parameters.genre){
-                    dataToSend+=`Genre: ${movie.Genre}.\n`;
+                    dataToSend+=`👾Genre: ${movie.Genre}.\n`;
                 }
                 if (req.body.queryResult.parameters.language){
-                    dataToSend+=`Language: ${movie.Language}.\n`;
+                    dataToSend+=`🌍Language: ${movie.Language}.\n`;
                 }
                 if (req.body.queryResult.parameters.runtime){
-                    dataToSend+=`Runtime: ${movie.Runtime}.\n`;
+                    dataToSend+=`🍿Runtime: ${movie.Runtime}.\n`;
                 }
                 if (req.body.queryResult.parameters.year){
-                    dataToSend+=`Year: ${movie.Year}.\n`;
+                    dataToSend+=`📆Year: ${movie.Year}.\n`;
                 }
                 if (req.body.queryResult.parameters.director){
-                    dataToSend+=`Director: ${movie.Director}.\n`;
+                    dataToSend+=`📽️Director: ${movie.Director}.\n`;
                 }
                 if (req.body.queryResult.parameters.actors){
-                    dataToSend+=`Actors: ${movie.Actors}.\n`;
+                    dataToSend+=`🎭Actors: ${movie.Actors}.\n`;
                 }
                 if (req.body.queryResult.parameters.plot){
-                    dataToSend+=`Plot: ${movie.Plot}\n`;
+                    dataToSend+=`📖Plot: ${movie.Plot}\n`;
                 }
                 if (req.body.queryResult.parameters.rating){
-                    dataToSend+=`Rating: ${movie.imdbRating}.\n`;
+                    dataToSend+=`📈Rating: ${movie.imdbRating}.\n`;
                 }
                 if (req.body.queryResult.parameters.awards){
-                    dataToSend+=`Awards: ${movie.Awards}.\n`;
+                    dataToSend+=`🏆Awards: ${movie.Awards}.\n`;
                 }
                 if (!req.body.queryResult.parameters.genre && !req.body.queryResult.parameters.year && !req.body.queryResult.parameters.director && !req.body.queryResult.parameters.actors && !req.body.queryResult.parameters.plot && !req.body.queryResult.parameters.rating && !req.body.queryResult.parameters.language && !req.body.queryResult.parameters.runtime && !req.body.queryResult.parameters.awards){
                         dataToSend+=`👾Genre: ${movie.Genre}.\n🌍Language: ${movie.Language}.\n🍿Runtime: ${movie.Runtime}.\n📆Year: ${movie.Year}.\n📽️Director: ${movie.Director}.\n🎭Actors: ${movie.Actors}.\n📖Plot: ${movie.Plot}\n📈Rating: ${movie.imdbRating}.\n🏆Awards: ${movie.Awards}\n${movie.Poster}`;
                 }
             }else{
-                dataToSend += `I don't have the required info on that.\n`;
+                dataToSend += `I don't have the required info on that. Please check that the data entered is correct.\n`;
             };
             return res.json({
                 fulfillmentText: dataToSend,
