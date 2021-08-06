c399b6112935e33d48cff5743887b3e2b8d034d3
niubility-57 <68056715+niubility-57@users.noreply.github.com>
2021-08-06 12:33:20 +0300

    Add new design for message

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const http = require('http');
-const API_KEY = require('./apiKey');
-
+const API_KEY = '157f9eb7';
 const server = express();
 server.use(bodyParser.urlencoded({
     extended: true
@@ -13,8 +12,35 @@ server.use(bodyParser.urlencoded({
 server.use(bodyParser.json());
 
 server.post('/get-movie-details', (req, res) => {
-
-    const movieToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.movie ? req.body.result.parameters.movie : 'The Godfather';
+    
+    if(req.body.queryResult.parameters.plot){
+        const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'INCORRECT';
+        
+        const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}&plot=full`);
+        http.get(reqUrl, (responseFromAPI) => {
+        let completeResponse = '';
+        responseFromAPI.on('data', (chunk) => {
+            completeResponse += chunk;
+        });
+        responseFromAPI.on('end', () => {
+            const movie = JSON.parse(completeResponse);
+            let dataToSend = `📖The plot for the ${movie.Title}: \n${movie.Plot}`;
+            
+            return res.json({
+                fulfillmentText: dataToSend,
+                source: 'get-movie-details',
+            });
+        });
+        }, (error) => {
+        return res.json({
+            fulfillmentText: 'Something went wrong!',
+            source: 'get-movie-details'
+        });
+    });
+            
+    }else{
+    
+    const movieToSearch = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.movie ? req.body.queryResult.parameters.movie : 'INCORRECT';
     const reqUrl = encodeURI(`http://www.omdbapi.com/?t=${movieToSearch}&apikey=${API_KEY}`);
     http.get(reqUrl, (responseFromAPI) => {
         let completeResponse = '';
@@ -23,22 +49,60 @@ server.post('/get-movie-details', (req, res) => {
         });
         responseFromAPI.on('end', () => {
             const movie = JSON.parse(completeResponse);
-            let dataToSend = movieToSearch === 'The Godfather' ? `I don't have the required info on that. Here's some info on 'The Godfather' instead.\n` : '';
-            dataToSend += `${movie.Title} is a ${movie.Actors} starer ${movie.Genre} movie, released in ${movie.Year}. It was directed by ${movie.Director}`;
+            let dataToSend = '';
+            if (movieToSearch === 'INCORRECT'){
+                dataToSend += `I don't have the required info on that. Please enter a movie title if you want to know more about it.\nFor example: What do you know about "Suicide squad"?\n`;
+            }else{
+                if (movie.Title){
+                    
+                    dataToSend +=`🎬Title: ${movie.Title}.\n`;
+                    
+                    if (req.body.queryResult.parameters.genre){
+                        dataToSend+=`👾Genre: ${movie.Genre}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.language){
+                        dataToSend+=`🌍Language: ${movie.Language}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.runtime){
+                        dataToSend+=`🍿Runtime: ${movie.Runtime}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.year){
+                        dataToSend+=`📆Year: ${movie.Year}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.director){
+                        dataToSend+=`📽️Director: ${movie.Director}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.actors){
+                        dataToSend+=`🎭Actors: ${movie.Actors}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.rating){
+                        dataToSend+=`📈Rating: ${movie.imdbRating}.\n`;
+                    }
+                    if (req.body.queryResult.parameters.awards){
+                        dataToSend+= `🏆Awards: ${movie.Awards}.\n`;
+                    }
 
+                    if (!req.body.queryResult.parameters.genre && !req.body.queryResult.parameters.year && !req.body.queryResult.parameters.director && !req.body.queryResult.parameters.actors && !req.body.queryResult.parameters.plot && !req.body.queryResult.parameters.rating && !req.body.queryResult.parameters.language && !req.body.queryResult.parameters.runtime && !req.body.queryResult.parameters.awards){
+                            dataToSend+=`👾Genre: ${movie.Genre}.\n🌍Language: ${movie.Language}.\n🍿Runtime: ${movie.Runtime}.\n📆Year: ${movie.Year}.\n📽️Director: ${movie.Director}.\n🎭Actors: ${movie.Actors}.\n📖Plot: ${movie.Plot}\n📈Rating: ${movie.imdbRating}.\n🏆Awards: ${movie.Awards}\n${movie.Poster}`;
+                    }
+                }else{
+                    dataToSend += `I don't have the required info on that. Please check that the data entered is correct.\n`;
+                }; 
+            };
+            
+            
             return res.json({
-                speech: dataToSend,
-                displayText: dataToSend,
-                source: 'get-movie-details'
+                fulfillmentText: dataToSend,
+                source: 'get-movie-details',
             });
         });
     }, (error) => {
         return res.json({
-            speech: 'Something went wrong!',
-            displayText: 'Something went wrong!',
+            fulfillmentText: 'Something went wrong!',
             source: 'get-movie-details'
         });
     });
+    };
 });
 
 server.listen((process.env.PORT || 8000), () => {
